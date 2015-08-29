3de5715f6fab08ae8b122c11599f0be72c62784e
Jonathan Stowe <jns+git@gellyfish.co.uk>
2015-08-29 11:11:00 +0100

    Add levels, message

--- lib/Lumberjack.pm
+++ lib/Lumberjack.pm
@@ -4,6 +4,17 @@ use Staticish;
 
 class Lumberjack {
 
+    enum Level <Off Fatal Error Warn Info Debug Trace All>;
+
+    class Message {
+        has Level $.level;
+        has Backtrace $.backtrace;
+        has Str $.message;
+
+        method gist {
+            "[{ $!level.Str.uc }] { $!message }";
+        }
+    }
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
