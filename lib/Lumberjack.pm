70dd76b3517d7301fa4194054b86373936a953fc
Jonathan Stowe <jns+git@gellyfish.co.uk>
2016-03-25 23:28:02 +0000

    :-\

--- lib/Lumberjack.pm
+++ lib/Lumberjack.pm
@@ -1,8 +1,12 @@
-use v6;
+use v6.c;
 
 use Staticish;
 
-class Lumberjack {
+class Lumberjack is Static {
+
+
+    has Supplier $!supplier;
+
 
     enum Level <Off Fatal Error Warn Info Debug Trace All>;
 
@@ -14,7 +18,30 @@ class Lumberjack {
         method gist {
             "[{ $!level.Str.uc }] { $!message }";
         }
+
     }
+
+    role Logger {
+        my Level $level;
+
+        method log-level() returns Level is rw {
+            $level;
+        }
+
+        proto method log(|c) { * }
+
+        multi method logx(Message $message) {
+            Lumberjack.log($message);
+        }
+
+        multi method log(Level $level, Str $message) {
+            my $backtrace = Backtrace.new;
+            my $mess = Message.new(:$level, :$message, :$backtrace);
+            samewith $message;
+        }
+
+    }
+
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
