fb585fc1ae9263b4793802fee20a0b8de1b04365
Jonathan Stowe <jns+git@gellyfish.co.uk>
2016-03-26 17:51:39 +0000

    Now it can do something approximates to logging

--- lib/Lumberjack.pm
+++ lib/Lumberjack.pm
@@ -4,21 +4,59 @@ use Staticish;
 
 class Lumberjack is Static {
 
+    class Message { ... };
 
-    has Supplier $!supplier;
+    has Supplier $!supplier     = Supplier.new;
+    has Supply   $.all-messages = $!supplier.Supply;
+
+    enum Level <Off Fatal Error Warn Info Debug Trace All> does role {
+        multi method ACCEPTS(Message $m) {
+            $m.level == self;
+        }
+    };
+
+    has Supply $.fatal-messages = $!all-messages.grep(Fatal);
+    has Supply $.error-messages = $!all-messages.grep(Error);
+    has Supply $.warn-messages  = $!all-messages.grep(Warn);
+    has Supply $.info-messages  = $!all-messages.grep(Info);
+    has Supply $.debug-messages = $!all-messages.grep(Debug);
+    has Supply $.trace-messages = $!all-messages.grep(Trace);
 
 
-    enum Level <Off Fatal Error Warn Info Debug Trace All>;
+    has Level $.default-level is rw = Error;
 
     class Message {
-        has Level $.level;
-        has Backtrace $.backtrace;
-        has Str $.message;
+        has Mu          $.class;
+        has Level       $.level;
+        has Backtrace   $.backtrace;
+        has Str         $.message is required;
+        has DateTime    $.when;
 
-        method gist {
-            "[{ $!level.Str.uc }] { $!message }";
+        multi method ACCEPTS(Level $l) {
+            $!level == $l;
         }
 
+        method Numeric() {
+            $!level;
+        }
+
+        submethod BUILD(:$!class, Level :$!level, Backtrace :$!backtrace, Str :$!message!) is hidden-from-backtrace {
+            if not $!level.defined {
+                $!level = Lumberjack.default-level;
+            }
+            if not $!backtrace.defined {
+                $!backtrace = Backtrace.new;
+            }
+            $!when = DateTime.now;
+        }
+        method gist {
+            "{$!when} [{ $!level.Str.uc }] { $!message }";
+        }
+
+    }
+
+    method log(Message $message) {
+        $!supplier.emit($message);
     }
 
     role Logger {
@@ -30,18 +68,32 @@ class Lumberjack is Static {
 
         proto method log(|c) { * }
 
-        multi method logx(Message $message) {
+        multi method log(Message $message) is hidden-from-backtrace {
             Lumberjack.log($message);
         }
 
-        multi method log(Level $level, Str $message) {
+        multi method log(Level $level, Str $message) is hidden-from-backtrace {
             my $backtrace = Backtrace.new;
-            my $mess = Message.new(:$level, :$message, :$backtrace);
-            samewith $message;
+            my $class = $?CLASS;
+            my $mess = Message.new(:$level, :$message, :$backtrace, :$class);
+            samewith $mess;
         }
 
     }
 
+    has Supply $.filtered-messages = supply {
+        whenever $!all-messages -> $m {
+            my $filter-level = do if $m.class ~~ Logger {
+                $m.class.log-level;
+            }
+            else {
+                $!default-level;
+            }
+            if $m.level <= $filter-level {
+                emit $m;
+            }
+        }
+    }
 }
 
 # vim: expandtab shiftwidth=4 ft=perl6
